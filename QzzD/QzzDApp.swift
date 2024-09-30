7d9167bf2230f7a73ff667a0e2a24eac677ecd9b
Mac <maclong9@icloud.com>
2024-09-30 15:09:50 +0100

    Initial Commit

---
+++ QzzD/QzzDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QzzDApp.swift
+//  QzzD
+//
+//  Created by Mac Long on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct QzzDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QzzDTests/QzzDTests.swift
@@ -0,0 +1,17 @@
+//
+//  QzzDTests.swift
+//  QzzDTests
+//
+//  Created by Mac Long on 30/09/2024.
+//
+
+import Testing
+@testable import QzzD
+
+struct QzzDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
