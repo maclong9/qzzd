2753bd6db42da5b290b6ba62f2810ac2d4ebd9a8
Mac <maclong9@icloud.com>
2024-10-01 23:38:09 +0100

    feat(Views/Layout): add `title` property

--- QzzD/Views/Components/Layout.swift
+++ QzzD/Views/Components/Layout.swift
@@ -9,27 +9,29 @@ import SwiftUI
 
 struct Layout<Content: ViewContent>: View {
     let content: () -> Content
+    let title: String
     
-    init(@ViewBuilder content: @escaping () -> Content) {
+    init(title: String = "", @ViewBuilder content: @escaping () -> Content) {
+        self.title = title
         self.content = content
     }
     
     var body: some View {
         VStack {
             Text("QzzD!")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .padding(.top, 10)
-            Spacer().frame(height: 50)
+                .font(.system(size: 56, weight: .black))
+                .padding(.vertical, 10)
+            Text(title)
+                .font(.system(size: 32, weight: .black))
             content()
-            Spacer()
         }
         .padding()
     }
 }
 
 #Preview {
-    Layout {
+    Layout(title: "Hello, World!") {
+        Spacer()
         Text("Hello, Layout!")
     }
 }
