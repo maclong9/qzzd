dcd8243a94cce86d630a73dcd38304574d4ef099
Mac <maclong9@icloud.com>
2024-10-01 21:41:15 +0100

    feat(Modes/Question): add Question structure

--- QzzD/Models/Question.swift
+++ QzzD/Models/Question.swift
@@ -5,3 +5,14 @@
 //  Created by Mac Long on 30/09/2024.
 //
 
+enum Style { case multiple, boolean }
+enum Difficulty { case easy, medium, hard }
+
+struct Question {
+    let type: Style
+    let question: String
+    let difficulty: Difficulty
+    let category: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+}
