113b5c2afdd2c09020d75927c9954812583abcec
Mac <maclong9@icloud.com>
2024-10-01 22:02:00 +0100

    feat(Question): make identifiable for ID tracking

--- QzzD/Models/Question.swift
+++ QzzD/Models/Question.swift
@@ -5,10 +5,13 @@
 //  Created by Mac Long on 30/09/2024.
 //
 
+import Foundation
+
 enum Style { case multiple, boolean }
 enum Difficulty { case easy, medium, hard }
 
-struct Question {
+struct Question: Identifiable {
+    let id: UUID
     let type: Style
     let question: String
     let difficulty: Difficulty
