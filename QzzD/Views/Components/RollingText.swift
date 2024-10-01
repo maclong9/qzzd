810f2bebbcd8ce76422ebf0798eeb05b6318eb81
Mac <maclong9@icloud.com>
2024-10-01 23:38:29 +0100

    feat(Views/NextRound): implement NextRound and RollingText Views

---
+++ QzzD/Views/Components/RollingText.swift
@@ -0,0 +1,61 @@
+//
+//  RollingText.swift
+//  QzzD
+//
+//  Created by Mac Long on 01/10/2024.
+//
+
+import SwiftUI
+
+struct RollingText: View {
+    @Binding var round: Int
+    @State private var animationRange: [Int] = []
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<animationRange.count, id: \.self) { index in
+                Text("0")
+                    .font(.system(size: 150, weight: .black))
+                    .opacity(0)
+                    .overlay {
+                        GeometryReader { proxy in
+                            let size = proxy.size
+                            
+                            VStack(spacing: 0) {
+                                ForEach(0...9, id: \.self) { number in
+                                    Text("\(number)")
+                                        .font(.system(size: 150, weight: .black))
+                                        .frame(width: size.width, height: size.height, alignment: .center)
+                                }
+                            }
+                            .offset(y: -CGFloat(animationRange[index]) * size.height)
+                        }
+                        .clipped()
+                    }
+            }
+        }
+        .onAppear {
+            initializeAnimationRange()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                updateText()
+            }
+        }
+    }
+    
+    private func initializeAnimationRange() {
+        animationRange = Array(repeating: 0, count: "\(round)".count)
+    }
+    
+    private func updateText() {
+        let stringValue = "\(round)"
+        for (index, value) in zip(0..<stringValue.count, stringValue) {
+            withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 1, blendDuration: 1)) {
+                animationRange[index] = Int(String(value)) ?? 0
+            }
+        }
+    }
+}
+
+#Preview {
+    NextRoundView(round: 954)
+}
--- QzzD/Views/Game/NextRound.swift
+++ QzzD/Views/Game/NextRound.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
-struct NextRound: View {
+struct NextRoundView: View {
+    var isReader: Bool = false
+    @State var round: Int
+    @State private var rotationAngle: Double = 0
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Layout(title: "Get Ready") {
+            Spacer().frame(height: 125)
+            RollingText(round: $round)
+            Spacer()
+            if isReader {
+                CButton(action: { print("Reading Question") }, fullWidth: true) {
+                    Text("See Question")
+                    Image(systemName: "arrowshape.turn.up.forward.fill")
+                }
+            } else {
+                CButton(action: { print("Waiting...") }, fullWidth: true, disabled: true) {
+                    Text("Waiting")
+                    Image(systemName: "rays")
+                }.disabled(true).opacity(0.8)
+            }
+        }
     }
 }
 
-#Preview {
-    NextRound()
+#Preview("Single Digit") {
+    NextRoundView(round: 8)
+}
+
+#Preview("Double Digit") {
+    NextRoundView(round: 64)
+}
+
+#Preview("Triple Digit") {
+    NextRoundView(round: 256)
 }
