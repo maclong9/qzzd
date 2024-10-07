c4451f82941fec7512fd8092254cae96355fad94
Mac <maclong9@icloud.com>
2024-10-07 16:53:09 +0100

    feat(Models/Game): move questions into init function

--- QzzD/Models/Game.swift
+++ QzzD/Models/Game.swift
@@ -19,34 +19,32 @@ struct Game: Identifiable {
     init(title: String, players: [Player] = [], roundCount: Int = 0, questionCount: Int = 1, currentReader: String? = nil) {
         self.title = title
         self.players = players
-        self.questions = questionsArray
         self.roundCount = roundCount
         self.questionCount = questionCount
         self.currentReader = currentReader
+        self.questions = [
+            Question(
+                style: .boolean,
+                difficulty: .easy,
+                category: .science,
+                question: "Apple designed the MacBook",
+                correctAnswer: .boolean(true)
+            ),
+            Question(
+                style: .normal,
+                difficulty: .medium,
+                category: .science,
+                question: "Who made the original Apple Computer?",
+                correctAnswer: .string("Steve Wozniak"),
+                incorrectAnswers: ["Steve Jobs", "Barry Allen", "Elon Musk"]
+            ),
+            Question(
+                style: .boolean,
+                difficulty: .hard,
+                category: .science,
+                question: "Steve Jobs threw an iPod in a fish tank",
+                correctAnswer: .boolean(true)
+            )
+        ]
     }
-    
-    private let questionsArray: [Question] = [
-        Question(
-            style: .boolean,
-            difficulty: .easy,
-            category: .science,
-            question: "Apple designed the MacBook",
-            correctAnswer: .boolean(true)
-        ),
-        Question(
-            style: .normal,
-            difficulty: .medium,
-            category: .science,
-            question: "Who made the original Apple Computer?",
-            correctAnswer: .string("Steve Wozniak"),
-            incorrectAnswers: ["Steve Jobs", "Barry Allen", "Elon Musk"]
-        ),
-        Question(
-            style: .boolean,
-            difficulty: .hard,
-            category: .science,
-            question: "Steve Jobs threw an iPod in a fish tank",
-            correctAnswer: .boolean(true)
-        )
-    ]
 }
